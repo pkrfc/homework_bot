8db2fac5d0e24e600de89c4ace20ee4ebe1f8f3a
pkrfc <mkorolev91@gmail.com>
2022-01-21 16:38:02 +0300

    v 4.3

--- homework.py
+++ homework.py
@@ -25,12 +25,33 @@ HOMEWORK_STATUSES = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+
+class TelegramLogsHandler(logging.Handler):
+    """Логи в чатик."""
+
+    def __init__(self, telegram_chat_id):
+        """Init."""
+        super().__init__()
+        self.chat_id = telegram_chat_id
+        self.bot = telegram.Bot(token=TELEGRAM_TOKEN)
+
+    def emit(self, record):
+        """Emit."""
+        log_entry = self.format(record)
+        self.bot.send_message(chat_id=self.chat_id, text=log_entry)
+
+
 logging.basicConfig(
     level=logging.DEBUG,
     filename='ya_bot.log',
     format='%(asctime)s - %(levelname)s - %(message)s - %(name)s'
 )
 logger = logging.getLogger(__name__)
+logger.addHandler(
+
+    TelegramLogsHandler(TELEGRAM_CHAT_ID)
+
+)
 
 
 class RequestError(Exception):
